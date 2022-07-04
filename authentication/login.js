984b03c1edc173dc0f8ca4b769f895cecacadf63
jayson7 <lexxiijoo70@gmail.com>
2022-07-04 23:52:45 +0100

    authentiation

--- authentication/login.js
+++ authentication/login.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar, StyleSheet} from 'react-native';
 
 export default function Login() {
   return (
-    <View>
-      <Text> Login Please</Text>
+    <View style={styles.loginContainer}>
+      <StatusBar barStyle="light-content" backgroundColor="#ffffff" />
+      <Text> Login Please </Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  loginContainer: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+  },
+});
