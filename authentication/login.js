59836657cbb01f341069925d3ebea631f27eaeee
jayson7 <lexxiijoo70@gmail.com>
2022-07-05 05:37:23 +0100

    authentiation

--- authentication/login.js
+++ authentication/login.js
@@ -9,60 +9,65 @@ import {
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 export default function Login() {
   return (
-    // login screen wrapper component
-    <View style={styles.loginContainer}>
-      {/* status bar configuration */}
-      <StatusBar barStyle="light-content" backgroundColor="#ffffff" />
-      {/* login screen inner component */}
+    <KeyboardAwareScrollView
+      style={{backgroundColor: '#061135'}}
+      resetScrollToCoords={{x: 0, y: 0}}
+      contentContainerStyle={styles.container}
+      scrollEnabled={true}>
+      <View style={styles.loginContainer}>
+        {/* status bar configuration */}
+        <StatusBar barStyle="light-content" backgroundColor="#ffffff" />
+        {/* login screen inner component */}
 
-      <View>
-        <Image
-          source={require('../images/login.png')}
-          style={styles.imageLogin}
-        />
-      </View>
-      <View>
-        <Text style={styles.textLogin}>Welcome Back</Text>
-      </View>
-      <View style={styles.loginSec}>
-        <View style={styles.Username}>
-          <Icon
-            name="user"
-            size={25}
-            color="#101292"
-            style={styles.Username_icon}
-          />
-          <TextInput
-            style={styles.Usernameinput}
-            placeholder="Username"
-            placeholderTextColor="#2d303b"
-            autoCapitalize="none"
-            autoCorrect={false}
+        <View>
+          <Image
+            source={require('../images/login.png')}
+            style={styles.imageLogin}
           />
         </View>
-        <View style={styles.Password}>
-          <Icon
-            name="lock"
-            size={25}
-            color="#101292"
-            style={styles.Password_icon}
-          />
-          <TextInput
-            style={styles.PasswordInput}
-            placeholder="Password"
-            placeholderTextColor="#2d303b"
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry={true}
-          />
+        <View>
+          <Text style={styles.textLogin}>Welcome Back</Text>
         </View>
+        <View style={styles.loginSec}>
+          <View style={styles.Username}>
+            <Icon
+              name="user"
+              size={25}
+              color="#101292"
+              style={styles.Username_icon}
+            />
+            <TextInput
+              style={styles.Usernameinput}
+              placeholder="Username"
+              placeholderTextColor="#2d303b"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.Password}>
+            <Icon
+              name="lock"
+              size={25}
+              color="#101292"
+              style={styles.Password_icon}
+            />
+            <TextInput
+              style={styles.PasswordInput}
+              placeholder="Password"
+              placeholderTextColor="#2d303b"
+              autoCapitalize="none"
+              autoCorrect={false}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        {/* login screen inner component ends here */}
       </View>
-      {/* login screen inner component ends here */}
-    </View>
-    // login screen wrapper component ends here
+    </KeyboardAwareScrollView>
   );
 }
 const styles = StyleSheet.create({
